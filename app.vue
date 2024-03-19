8c25e2b9b25f945f60a6e2064c962a4ccf1772cd
Sergey Timoshenko <88145578+Zmeev06@users.noreply.github.com>
2024-03-19 18:46:39 +0300

    first page

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
-  </div>
+		<NuxtPage />
+	</div>
 </template>
 
 <script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+  
+</script>
+<template lang='html'>
+  <div>
+    hello
+  </div>
+</template>
+<style lang='scss'>
+    
+</style>
