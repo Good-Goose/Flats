5ed9ebd4fc8e772b1e45ca29e30fed36451f4c36
Sergey Timoshenko <88145578+Zmeev06@users.noreply.github.com>
2024-03-20 12:42:18 +0300

    feat: CatalogLayout

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
-<script setup lang='ts'>
-  
+<script setup lang="ts">
+
+import CatalogLayout from 'shared/layouts/CatalogLayout/ui/CatalogLayout.vue';
 </script>
-<template lang='html'>
-  <div>
-    hello
-  </div>
+<template lang="html">
+  <CatalogLayout>
+    
+  </CatalogLayout>
 </template>
 <style lang='scss'>
     
---
+++ src/shared/layouts/CatalogLayout/ui/CatalogLayout.vue
@@ -0,0 +1,9 @@
+<script setup lang='ts'>
+
+</script>
+<template lang='html'>
+  <div class=" max-w-[86vw] mx-auto">
+    <slot />
+  </div>
+</template>
+<style lang='scss'></style>
