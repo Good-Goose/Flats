363fe141bc8463e28fc08c345da83af992965e05
Nikita Lomeiko <87067770+lomeiko-dev@users.noreply.github.com>
2024-03-27 18:27:41 +0300

    feat: filters

---
+++ src/features/FilterFlats/ui/LinkItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface IProps {
+  title: string;
+  link: string;
+}
+
+const props = defineProps<IProps>();
+</script>
+<template>
+  <div>
+    <a
+      @click="() => console.log(link)"
+      class="text-base font-bold text-dark text-nowrap border-b border-solid border-gray-dark-1 px-1 py-[10px] duration-200 hover:pb-[14px] hover:border-dark"
+      href="#"
+      >{{ props.title }}</a
+    >
+  </div>
+</template>
+<style lang="scss"></style>
